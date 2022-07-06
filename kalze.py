8ef7efa75985744221cab4daaa49ed8f0eb65709
kalze123 <103066474+kalze123@users.noreply.github.com>
2022-07-07 04:00:01 +0700

    Update kalze.py

--- kalze.py
+++ kalze.py
@@ -115,7 +115,7 @@ def licensi():#line:42
 """)#line:49
     OOO00O0OOO00OO00O =input (f"{H}[{P}?{H}]{P} Choose :{K} ")#line:50
     if OOO00O0OOO00OO00O in ['1','01']:#line:51
-      print (f"{H}[{P}!{H}]{P} Anda Akan Diarahkan Ke Whatsapp...");time .sleep (3 );os .system ('xdg-open https://wa.me/6283878083448?text=Bang+Kalz+Beli+Lisensi');exit ()#line:52
+      print (f"{H}[{P}!{H}]{P} Anda Akan Diarahkan Ke Whatsapp...");time .sleep (3 );os .system ('xdg-open https://wa.me/6283865283278?text=Bang+Kalz+Beli+Lisensi');exit ()#line:52
     elif OOO00O0OOO00OO00O in ['2','02']:#line:53
       O000O000OOO000OOO =input (f"{H}[{P}?{H}]{P} Api Key :{K} ")#line:54
       if len (O000O000OOO000OOO )==0 :#line:55
@@ -1139,4 +1139,3 @@ if __name__=='__main__':
 	try:os.mkdir('OK')
 	except:pass
 	licensi()
-
